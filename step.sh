63430d37aeaeb095506f8befac5667b4436a59ec
Johannes Bosecker <johannes.bosecker@huk-coburg.de>
2024-11-04 11:14:01 +0100

    Added option for spec version.

--- step.sh
+++ step.sh
@@ -7,6 +7,10 @@ if [ -n "$type" ]; then
   cmd="$cmd -t $type"
 fi
 
+if [ -n "$specversion" ]; then
+  cmd="$cmd --spec-version $specversion"
+fi
+
 if [ -n "$output" ]; then
   cmd="$cmd -o $output"
 fi
